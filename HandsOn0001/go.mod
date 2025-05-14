9e523f8f51900cb779c484c5825f5759aafed9a9
Pep Lainez <jlainezs@gmail.com>
2025-05-14 07:33:39 +0200

    Reorganize directories.

---
+++ HandsOn0001/go.mod
@@ -0,0 +1,3 @@
+module HandsOn0001
+
+go 1.24
---
+++ HandsOn0001/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// main is the entry point of the program,
+// printing a separator and a greeting message to the console.
+func main() {
+	fmt.Println("---")
+	fmt.Println("Hello, World!")
+}
