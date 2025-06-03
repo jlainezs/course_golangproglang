850ad13d1f2412b607081bd85d8a90d7e5d6e504
Pep Lainez <jlainezs@gmail.com>
2025-06-03 08:03:55 +0200

    Exercise 26

---
+++ 012/0026/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func myIntN() int {
+	return rand.Intn(250)
+}
+
+func main() {
+	x := myIntN()
+	fmt.Printf("x is %d\n", x)
+	switch {
+	case x >= 0 && x <= 100:
+		fmt.Println("x is between 0 and 100")
+	case x >= 101 && x <= 200:
+		fmt.Println("x is between 101 and 200")
+	case x >= 201 && x <= 250:
+		fmt.Println("x is between 201 and 250")
+	default:
+		fmt.Println("It should not show this message.")
+	}
+}
+
+func init() {
+	fmt.Println("This is were initialization goes.")
+}
---
+++ 012/0026/readme.md
@@ -0,0 +1,3 @@
+# Exercise 26
+Modify the previous program to have your program use the init func to print
+- "This is where initialization for my program occurs
