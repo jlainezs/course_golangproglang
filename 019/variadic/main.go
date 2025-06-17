66554b78d4f510d1986055b799d0c1a0766fe0a1
Pep Lainez <jlainezs@gmail.com>
2025-06-17 07:57:03 +0200

    Call function with variadic parameters using an slice.

--- 019/variadic/main.go
+++ 019/variadic/main.go
@@ -15,4 +15,7 @@ func main() {
 	fmt.Println(sum(1))
 	fmt.Println(sum(1, 2, 3))
 	fmt.Println(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println(sum(nums...))
 }
