51d3cbda10b180ce87134aa308fe0eb2d3227ada
Pep Lainez <jlainezs@gmail.com>
2025-06-17 07:55:59 +0200

    Variadic parameter in function signature.

---
+++ 019/variadic/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func sum(numbers ...int) int {
+	sum := 0
+	for _, n := range numbers {
+		sum += n
+	}
+
+	return sum
+}
+
+func main() {
+	fmt.Println(sum(1))
+	fmt.Println(sum(1, 2, 3))
+	fmt.Println(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+}
