003d9e4144ee4a1890bfd56ecf8227be69c564a3
Pep Lainez <jlainezs@gmail.com>
2025-06-19 07:23:52 +0200

    Methods and receivers.

--- 019/methods_receivers/main.go
+++ 019/methods_receivers/main.go
@@ -1,1 +1,21 @@
-package methods_receivers
+package main
+
+import "fmt"
+
+type person struct {
+	name string
+}
+
+// use a receiver to add methods to a type
+func (p person) speak() {
+	fmt.Println("speaking to", p.name)
+}
+
+func main() {
+	p1 := person{
+		name: "Bob",
+	}
+
+	// use the method speak defined in the struct person
+	p1.speak()
+}
