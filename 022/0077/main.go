1d537fa635716bd3001981c2cf9531f13cc353b2
Pep Lainez <jlainezs@gmail.com>
2025-06-24 10:24:47 +0200

    Exercise 77

--- 022/0077/main.go
+++ 022/0077/main.go
@@ -1,1 +1,29 @@
-package _077
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+}
+
+func changeValue(who person, newValue string) person {
+	who.first = newValue
+	return who
+}
+
+func changePtr(who *person, newValue string) {
+	who.first = newValue
+}
+
+func main() {
+	p := person{
+		first: "Perica",
+	}
+	fmt.Println(p)
+
+	otherP := changeValue(p, "Pollo")
+	fmt.Println(p, otherP)
+
+	changePtr(&p, "Pastrami")
+	fmt.Println(p)
+}
---
+++ 022/0077/readme.md
@@ -0,0 +1,10 @@
+# Hands-on exercise #77 - value & pointer semantics
+
+Create two functions to change a field in a struct called "first" of TYPE string:
+
+- One function will use VALUE SEMANTICS
+  - this function will return some VALUE of some TYPE
+- The other function will use POINTER SEMANTICS
+  - ○ this function will return nothing
+- don't use methods
+
