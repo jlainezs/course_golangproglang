415cee925aa640324b77a8bbd92c0044a891c95a
Pep Lainez <jlainezs@gmail.com>
2025-07-03 07:58:03 +0200

    More exercises.

--- 027/001/main.go
+++ 027/001/main.go
@@ -1,1 +1,26 @@
-package _01
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func sub1() {
+	defer wg.Done()
+	fmt.Printf("Sub 1\n")
+}
+
+func sub2() {
+	defer wg.Done()
+	fmt.Printf("Sub 2\n")
+}
+
+func main() {
+	wg.Add(2)
+	go sub1()
+	go sub2()
+	wg.Wait()
+	fmt.Printf("main finished\n")
+}
--- 027/002/main.go
+++ 027/002/main.go
@@ -1,1 +1,26 @@
-package _02
+package main
+
+import "fmt"
+
+type Person struct {
+	name string
+	age  int
+}
+
+type Human interface {
+	speak(whatToSay string)
+}
+
+func (p *Person) speak(whatToSay string) {
+	fmt.Printf("%s says %s\n", p.name, whatToSay)
+}
+
+func saySomething(h Human) {
+	h.speak("How do you do?")
+}
+
+func main() {
+	p := Person{"Bob", 20}
+	saySomething(&p) // this works
+	//saySomething(p)	// this does not works
+}
