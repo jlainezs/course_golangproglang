fffafc8ebd121aa360bda9a326f67988d66414b8
Pep Lainez <jlainezs@gmail.com>
2025-07-06 11:24:59 +0200

    Initial code for exercise 4

---
+++ 029/004/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	q := make(chan int)
+	c := gen(q)
+
+	receive(c, q)
+
+	fmt.Println("about to exit")
+}
+
+func gen(q <-chan int) <-chan int {
+	c := make(chan int)
+
+	for i := 0; i < 100; i++ {
+		c <- i
+	}
+
+	return c
+}
---
+++ 029/004/readme.md
@@ -0,0 +1,3 @@
+# Hands-on exercise #4
+
+- Starting with this code, pull the values off the channel using a select statement
