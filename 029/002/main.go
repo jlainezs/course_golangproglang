808c51cb19f273f1120cfa94750404f187bfb1e9
Pep Lainez <jlainezs@gmail.com>
2025-07-06 09:44:56 +0200

    One solution, keeping the receive only channel in a function.

--- 029/002/main.go
+++ 029/002/main.go
@@ -5,12 +5,15 @@ import (
 )
 
 func main() {
-	cs := make(chan<- int)
+	cs := make(chan int)
 
 	go func() {
 		cs <- 42
 	}()
-	fmt.Println(<-cs)
+
+	go func(c <-chan int) {
+		fmt.Println(<-cs)
+	}(cs)
 
 	fmt.Printf("------\n")
 	fmt.Printf("cs\t%T\n", cs)
