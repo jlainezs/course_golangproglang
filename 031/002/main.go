cf5d3f18ddcb56f0fe2f0259cb46abd05521fd14
Pep Lainez <jlainezs@gmail.com>
2025-07-08 08:14:27 +0200

    Instead of nil return an empty byte slice.

--- 031/002/main.go
+++ 031/002/main.go
@@ -32,7 +32,7 @@ func main() {
 func toJSON(a interface{}) ([]byte, error) {
 	bs, err := json.Marshal(a)
 	if err != nil {
-		return nil, fmt.Errorf("marshalling failed: %v", err)
+		return []byte{}, fmt.Errorf("marshalling failed: %v", err)
 	}
 
 	return bs, nil
